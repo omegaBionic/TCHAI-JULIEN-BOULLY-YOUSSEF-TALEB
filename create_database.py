24378ea8cf0d854bf6e65382797fd39fe95fb7e7
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2021-12-08 11:16:46 +0100

    [Core] Added auto conf.

--- create_database.py
+++ create_database.py
@@ -1,13 +1,14 @@
 import sqlite3
 from sqlite3 import Error
+import utils.config as Config
 
+config = Config.Config()
 
 try:
-    conn = sqlite3.connect('SQL_TCHAI.db')
+    conn = sqlite3.connect(config.config_data["database_config"]["database_name"])
     c = conn.cursor()
     c.execute('''
     CREATE TABLE tblTransactions ( id INTEGER PRIMARY KEY AUTOINCREMENT, sender VARCHAR(30) NOT NULL, receiver VARCHAR(30) NOT NULL, time_transaction VARCHAR(20) NOT NULL, money REAL NOT NULL);
-    
               ''')
     conn.commit()
 except Error as e:
---
+++ utils/config.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+
+class Config:
+    def __init__(self):
+        self.config_data = {}
+        self.conf_path = os.path.join("config.json")
+        try:
+            with open(self.conf_path) as json_data_file:
+                self.config_data = json.load(json_data_file)
+        except OSError as err:
+            print("OS error: {0}".format(err))
+        except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            raise
